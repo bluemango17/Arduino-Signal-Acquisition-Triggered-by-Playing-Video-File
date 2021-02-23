ce8afa9baa1d3750fc85b001c29411eb279fd541
bluemango17 <45534728+bluemango17@users.noreply.github.com>
2021-02-23 20:37:15 +0100

    Add files via upload

---
+++ analogReadBezKasnjenja.ino
@@ -0,0 +1,25 @@
+ /*
+  AnalogReadSerial + BlinkWithoutDelay
+  kombinacijom ova dva koda nastao je ovaj jedan
+  ideja je da frekvencija odabiranja bude fiksna, odnosno
+  da se na svakih interval milisekundi šalje po jedan
+  odbirak na serijski port, a da se on kasnije snimi u R-u.
+*/
+
+unsigned long previousMillis = 0;
+const long interval = 100; // ako je 10 znači da je sampling rate 100 Hz
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  Serial.println(voltage);
+  }
+}
